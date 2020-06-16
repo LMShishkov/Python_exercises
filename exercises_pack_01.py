3e316d029a98d8bc09bbbc5ffd1347c97d457da6
Lubomir Shishkov <l.m.shishkov@gmail.com>
2020-06-16 09:12:22 +0300

    Exercise 2 added

--- exercises_pack_01.py
+++ exercises_pack_01.py
@@ -1,4 +1,5 @@
-# EX. 1 Calculate down payment of a house which costs $1m, based on the credit rating:
+# EX. 1 Calculate down payment of a house which costs $1m, based on the credit rating. Utilizes - basic application of
+# built-in input() function along with if condition based on user input options:
 
 price = 1000000
 credit_rating = input('Is your credit rating good? True or False?: ')
@@ -7,3 +8,14 @@ if credit_rating == 'True':
 if credit_rating == 'False':
     down_payment = price*0.275
 print(f"Down payment: ${down_payment}")
+
+# EX. 2 Ask for user input and return a message based on the input. Utilizes - basic application of built-in input()
+# function along with if condition based on length:
+
+name = input("Type your name: ")
+if len(name) < 3:
+    print('Name must be at least 3 characters!')
+elif len(name) > 50:
+    print('Name can be a maximum of 50 characters!')
+else:
+    print('Name looks good!')
