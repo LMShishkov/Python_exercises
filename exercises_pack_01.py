1f0a5c5c21ad5cd4d3f29727e41a754d4a9a16b0
Lubomir Shishkov <l.m.shishkov@gmail.com>
2020-06-18 09:42:59 +0300

    Exercise 4 added

--- exercises_pack_01.py
+++ exercises_pack_01.py
@@ -9,6 +9,7 @@ if credit_rating == 'False':
     down_payment = price*0.275
 print(f"Down payment: ${down_payment}")
 
+
 # EX. 2 Ask for user input and return a message based on the input. Utilizes - basic application of built-in input()
 # function along with if condition based on length:
 
@@ -20,6 +21,7 @@ elif len(name) > 50:
 else:
     print('Name looks good!')
 
+
 # EX. 3 Simple converter with two options stored in if/else statement:
 
 weight = input("Weight: ")
@@ -28,3 +30,25 @@ if weight_type == 'K':
     print(int(weight) * 2.20)
 else:
     print(int(weight) * 0.453592)
+
+
+# EX. 4 Simple converter with more checks and two while loops
+
+dist_sys = ''
+dist = float()
+while dist <= 0:
+    dist = float(input('Enter distance> '))
+    if dist <=0:
+        print('Enter number bigger than 0')
+else:
+    while dist_sys != 'Km' and 'Mi':
+        dist_sys = input('Enter Km or Mi> ')
+        if dist_sys == 'Km':
+            dist = dist * 0.6214
+            print(f'{dist} miles')
+        elif dist_sys == "Mi":
+            dist = dist * 1.60934
+            print(f'{dist} kilometers')
+        else:
+            print('Invalid distance measure')
+
