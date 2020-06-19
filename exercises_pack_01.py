cb854a8971879b1be7f430c2de0742ef36f19bce
Lubomir Shishkov <l.m.shishkov@gmail.com>
2020-06-19 09:54:19 +0300

    Added Exercise 6

--- exercises_pack_01.py
+++ exercises_pack_01.py
@@ -72,3 +72,17 @@ if guess == number:
     print ('Good job, {0}! You guessed my number in {1} guesses!'.format(name, guesses_made))
 else:
     print ('Nope. The number I was thinking of was {0}'.format(number))
+
+# EX. 6 Guess the number game - version 2 (non-random generated number):
+
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+while guess_limit > guess_count:
+    guess = int(input("Guess: "))
+    guess_count += 1
+    if guess == secret_number:
+        print("You won!")
+        break
+else:
+    print("Sorry you failed!")
