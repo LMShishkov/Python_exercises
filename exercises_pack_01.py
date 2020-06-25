55cef9979a137486f37002aa4423cf902a7e258e
Lubomir Shishkov <l.m.shishkov@gmail.com>
2020-06-25 18:47:15 +0300

    Exercise 19 added

--- exercises_pack_01.py
+++ exercises_pack_01.py
@@ -481,7 +481,7 @@ print('Students who took either (or both) of the exam and the project:', exam | 
 print('Students who took either (but not both) of the exam and the project:', exam ^ project)
 
 
-# EX. 18 In consists of input prompt together with a dictionary type Counter, example of how this dictionary is
+# EX. 18 It consists of input prompt together with a dictionary type Counter, example of how this dictionary is
 # accessed based on keys through c['cat'] and ordered dictionary based on the input of each word in the text
 
 import collections
@@ -495,3 +495,57 @@ c = collections.Counter(split_input)
 print(c['cat'])
 ordered_dict = OrderedDict(c)
 print(ordered_dict)
+
+# EX. 19 Stack + map and filter exercise
+
+class Stack:
+
+    def __init__(self):
+        self._list = []  # initial internal data
+
+    def push(self, element):
+        self._list.append(element)
+
+    def pop(self):
+        return self._list.pop()
+
+    def top(self):
+        return self._list[self.length() - 1]
+
+    def length(self):
+        return len(self._list)
+
+    def is_empty(self):
+        return self.length() == 0
+
+    # Makes the Stack iterable
+    def __iter__(self):
+        return iter(self._list)
+
+    def __str__(self):
+        return 'Stack: ' + str(self._list)
+
+
+# Define functiosn to use with map and filter
+def add_item(item):
+    return 'item: ' + item
+
+
+def is_job(item):
+    return item.startswith('Job')
+
+
+stack = Stack()
+stack.push('Task1')
+stack.push('Task2')
+stack.push('Job1')
+stack.push('Task3')
+stack.push('Job2')
+print('stack contents:', stack)
+
+# Apply functions to stack contents using map and filter
+new_list = list(map(add_item, stack))
+print('new_list:', new_list)
+
+filtered_list = list(filter(is_job, stack))
+print('filtered_list: ', filtered_list)
