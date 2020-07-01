82ad124167d4cf395082ff72cf00677a6e477e26
Lubomir Shishkov <l.m.shishkov@gmail.com>
2020-07-01 14:54:46 +0300

    Exercise 2 added to exercise pack 03

--- exercises_pack_03.py
+++ exercises_pack_03.py
@@ -33,3 +33,55 @@ d3 = Dog('Ben', 5)
 dogs = [d1, d2, d3]
 
 print(get_oldest_dog(dogs))
+
+
+# EX. 2Using the above class createa a separate class and make all instances of the first class also instances of the
+# second class. Also adopt class inheritance for the first class and instantiate objects from the sub-classes.
+# Without using a classmethod count the number of instances and display it.
+
+class Pets:
+
+    dogs = []
+
+    def __init__(self, dogs):
+        self.dogs = dogs
+
+
+class Dog:
+
+    species = 'mammal'
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def description(self):
+        return self.name, self.age
+
+    def speak(self, sound):
+        return "%s says %s" % (self.name, sound)
+
+class RussellTerrier(Dog):
+    def run(self, speed):
+        return "%s runs %s" % (self.name, speed)
+
+
+class Bulldog(Dog):
+    def run(self, speed):
+        return "%s runs %s" % (self.name, speed)
+
+
+my_dogs = [
+    Bulldog("Tom", 6),
+    RussellTerrier("Fletcher", 7),
+    Dog("Larry", 9)
+]
+
+my_pets = Pets(my_dogs)
+
+
+print("I have {} dogs.".format(len(my_pets.dogs)))
+for dog in my_pets.dogs:
+    print("{} is {}.".format(dog.name, dog.age))
+
+print("And they're all {}s, of course.".format(dog.species))
