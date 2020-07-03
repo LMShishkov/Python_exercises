cddd6d1bc149c88ed1a8cae726fb48eeb96fca9e
Lubomir Shishkov <l.m.shishkov@gmail.com>
2020-07-03 06:59:06 +0300

    Exercise 3 added to exercise pack 04

--- exercises_pack_04.py
+++ exercises_pack_04.py
@@ -1,7 +1,18 @@
-# Exercise 2: Area of a Room - Write a program that asks the user to enter the width and length of a room. Once the
-# values have been read, your program should compute and display the area of the room. The length and the width will
-# be entered as floating point numbers. Include units in your prompt and output message; either feet or meters,
-# depending on which unit you are more comfortable working with.
+# Exercise 3: Area of a Field - Create a program that reads the length and width of a farmer’s field from the user in
+# feet. Display the area of the field in acres.
+
+print('This is the field area calculator')
+field_width = float(input('Please enter the width of your field in feet: '))
+field_length = float(input('Please enter the length of your field in feet: '))
+field_area = (field_width * field_length) / 43.560
+formatted_field_area = '{:.2f}'.format(field_area)
+print(f'Your field area is {formatted_field_area} acres')
+
+
+# Exercise 2: Area of a Room - Write a program that asks the user to
+# enter the width and length of a room. Once the values have been read, your program should compute and display the
+# area of the room. The length and the width will be entered as floating point numbers. Include units in your prompt
+# and output message; either feet or meters, depending on which unit you are more comfortable working with.
 
 print('This is the room area calculator')
 room_width = float(input('Please enter the width of your room in meters: '))
