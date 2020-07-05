09ed58e277fffba90e129f57d33d0571f0929c2b
Lubomir Shishkov <l.m.shishkov@gmail.com>
2020-07-05 15:20:56 +0300

    Added exercise 7 in exercise pack 04

--- exercises_pack_04.py
+++ exercises_pack_04.py
@@ -1,4 +1,16 @@
-# Exercise 6: Exercise 6: Sum of the First n Positive Integers - Write a program that reads a positive integer, n,
+# Exercise 7: Widgets and Gizmos - An online retailer sells two products: widgets and gizmos. Each widget
+# weighs 75 grams. Each gizmo weighs 112 grams. Write a program that reads the number of widgets and the number of
+# gizmos in an order from the user. Then your program should compute and display the total weight of the order.
+
+print('The widgets and gizmos weight calculator')
+widgets, gizmos = input("Enter the amount of widgets and gizmos: ").split()
+widget_weight = 75
+gizmo_weight = 112
+total_weight = int(widgets)*widget_weight + int(gizmos)*gizmo_weight
+print(f'The total weight of your order is: {total_weight} grams')
+
+
+# Exercise 6: Sum of the First n Positive Integers - Write a program that reads a positive integer, n,
 # from the user and then displays the sum of all of the integers from 1 to n. The sum of the first n positive
 # integers can be computed using the formula: sum = (n)(n + 1)/2
 
