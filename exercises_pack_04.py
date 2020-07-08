0a904f7bd5ad67fa432e9ff131cd8f07c03b4d5d
Lubomir Shishkov <l.m.shishkov@gmail.com>
2020-07-08 15:12:05 +0300

    Added Exercise 10 in exercise pack 04

--- exercises_pack_04.py
+++ exercises_pack_04.py
@@ -1,3 +1,38 @@
+# Exercise 10: Making Change - Consider the software that runs on a self-checkout machine. One task that it must be
+# able to perform is to determine how much change to provide when the shopper pays for a purchase with cash. Write a
+# program that begins by reading a number of cents from the user as an integer. Then your program should compute and
+# display the denominations of the coins that should be used to give that amount of change to the shopper. The change
+# should be given using as few coins as possible. Assume that the machine is loaded with pennies, nickels, dimes,
+# quarters, loonies and toonies.
+
+print('The change cash-machine converter')
+CENTS_PER_TOONIE = 200
+CENTS_PER_LOONIE = 100
+CENTS_PER_QUARTER = 25
+CENTS_PER_DIME = 10
+CENTS_PER_NICKEL = 5
+
+change = float(input('Enter the number in $: '))
+cents = change*100
+
+print(' ', cents // CENTS_PER_TOONIE, 'toonies')
+cents = cents % CENTS_PER_TOONIE
+
+print(' ', cents // CENTS_PER_LOONIE, 'loonies')
+cents = cents % CENTS_PER_LOONIE
+
+print(' ', cents // CENTS_PER_QUARTER, 'quarters')
+cents = cents % CENTS_PER_QUARTER
+
+print(' ', cents // CENTS_PER_DIME, 'dimes')
+cents = cents % CENTS_PER_DIME
+
+print(' ', cents // CENTS_PER_NICKEL, 'nickels')
+cents = cents % CENTS_PER_NICKEL
+
+print(' ', cents, 'pennies')
+
+
 # Exercise 9: Fuel Converter - In the United States, fuel efficiency for vehicles is normally expressed in
 # miles-per-gallon (MPG). In Canada, fuel efficiency is normally expressed in liters-per-hundred kilometers (L/100
 # km). Use your research skills to determine how to convert from MPGto L/100 km. Then create a program that reads a
