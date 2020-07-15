700ed5cde51a4276074946b075e30bc5d53c57de
Lubomir Shishkov <l.m.shishkov@gmail.com>
2020-07-15 22:38:25 +0300

    Added exercise 31 to exercise pack 04

--- exercises_pack_04.py
+++ exercises_pack_04.py
@@ -1,3 +1,20 @@
+# Exercise 31: Name that Triangle - A triangle can be classified based on the lengths of its sides as equilateral,
+# isosceles or scalene. All 3 sides of an equilateral triangle have the same length. An isosceles triangle has two sides
+# that are the same length, and a third side that is a different length. If all of the sides have different lengths then
+# the triangle is scalene. Write a program that reads the lengths of 3 sides of a triangle from the user. Display a
+# message indicating the type of the triangle.
+
+print('Name that Triangle')
+side1, side2, side3 = input('Please input the length of the 3 triangle sides in cm: ').split()
+if side1 != side2 != side3:
+    print('The triangle is scalene')
+elif side1 == side2 == side3:
+    print('The triangle is equilateral')
+else:
+    print('The triangle is isosceles')
+
+
+
 # Exercise 30: Sound Levels - The following table (check notion.so link) lists the sound level in decibels for several
 # common noises. Write a program that reads a sound level in decibels from the user. If the user enters a decibel level
 # that matches one of the noises in the table then your program should display a message containing only that noise. If
