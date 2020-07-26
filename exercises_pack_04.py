799afec4c6781b48d95705710382ca5fa756e953
Lubomir Shishkov <l.m.shishkov@gmail.com>
2020-07-26 22:59:55 +0300

    Added Exercise 46 in exercise pack 04

--- exercises_pack_04.py
+++ exercises_pack_04.py
@@ -1,3 +1,32 @@
+# Exercise 46: No More Pennies - February 4, 2013was the last day that pennies were distributed by the Royal Canadian
+# Mint. Now that pennies have been phased out retailers must adjust totals so that they are multiples of 5 cents when
+# they are paid for with cash (credit card and debit card transactions continue to be charged to the penny). While
+# retailers have some freedomin how they do this, most choose to round to the closest nickel. Write a program that reads
+# prices from the user until a blank line is entered. Display the total cost of all the entered items on one line,
+# followed by the amount due if the customer pays with cash on a second line. The amount due for a cash payment should
+# be rounded to the nearest nickel. One way to compute the cash payment amount is to begin by determining how many
+# pennies would be needed to pay the total. Then compute the remainder when this number of pennies is divided by 5.
+# Finally, adjust the total down if the remainder is less than 2.5. Otherwise adjust the total up.
+
+total = 0
+rounded_total = 0
+line = input('Please input a all the prices (use a blank line if you are finished): ')
+while line != "":
+    total += float(line)
+    line = input('Please input a all the prices (use a blank line if you are finished): ')
+
+total_transformed = total * 100
+pennies_per_nickel = 5
+
+if total_transformed % pennies_per_nickel > 2:
+    rounded_total = total_transformed + (pennies_per_nickel - total_transformed % pennies_per_nickel)
+else:
+    rounded_total = total_transformed - total_transformed % pennies_per_nickel
+
+print('Amount due: {0:.2f}$'.format(total))
+print('Amount due in cash: {0:.2f}$'.format(rounded_total/100))
+
+
 # Exercise 45: Temperature Conversion Table - Write a program that displays a temperature conversion table for degrees
 # Celsius and degrees Fahrenheit. The table should include rows for all temperatures between 0 and 100 degrees Celsius
 # that are multiples of 10 degrees Celsius. Include appropriate headings on your columns. The formula for converting
