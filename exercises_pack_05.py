c80bdf74e607773c1e7257c139e9c762716bc66f
Lubomir Shishkov <l.m.shishkov@gmail.com>
2020-07-27 21:46:46 +0300

    Added exercise 8 in exercise pack 05

--- exercises_pack_05.py
+++ exercises_pack_05.py
@@ -1,3 +1,69 @@
+# Exercise 8: Write a separate Privileges class. The class should have one attribute, privileges, that stores a list
+# of strings as described in Exercise 7. Move the show_privileges() method to this class. Make a Privileges instance
+# as an attribute in the Admin class. Create a new instance of Admin and use your method to show its privileges.
+
+privileges_default = ['can add post', 'can delete post', 'can ban user']
+
+
+class Privileges():
+
+    def __init__(self, privileges=None):
+        if privileges is None:
+            privileges = privileges_default
+        self.privileges = privileges
+
+    def show_privileges(self):
+        print(f'Privileges for Admin User are: {self.privileges}')
+
+
+class User():
+    class_id = 0
+
+    @classmethod
+    def class_id_update(cls):
+        cls.class_id += 1
+
+    def __init__(self, first_name, last_name, status):
+        User.class_id_update()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.status = status
+        self.id = User.class_id
+        self.login_attempts = 0
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+        print(f'Resetting login for user ID: {self.id}...')
+
+    def describe_user(self):
+        print(f'User names:{self.first_name} {self.last_name} | User status: {self.status} | User ID: {self.id} | '
+              f'Login Attempts: {self.login_attempts}')
+
+    def greet_user(self):
+        print(f'Hello {self.first_name} {self.last_name}!')
+
+
+class Admin(User):
+
+    def __init__(self, first_name, last_name, status):
+        super().__init__(first_name, last_name, status)
+        User.class_id_update()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.status = status
+        self.id = User.class_id
+        self.login_attempts = 0
+        self.privileges_list = Privileges()
+
+
+privileges_default = ['can add post', 'can delete post', 'can ban user']
+user4 = Admin('Don', 'King', 'active')
+user4.privileges_list.show_privileges()
+
+
 # Exercise 7: An administrator is a special kind of user. Write a class called Admin that inherits from the User class
 # in Exercise 5 Add an attribute, privileges, that stores a list of strings like "can add post", "can delete post", "can
 # ban user", and so on. Write a method called show_privileges() that lists the administrator’s set of privileges. Create
