299d3a15bf82e8310ff1503e3972487687c83c91
Lubomir Shishkov <l.m.shishkov@gmail.com>
2020-09-04 09:44:59 +0300

    Added Exercise 66 to exercise pack 04

--- exercises_pack_04.py
+++ exercises_pack_04.py
@@ -1,3 +1,25 @@
+# Exercise 66:  Is it a Valid Triangle? - If you have 3 straws, possibly of differing lengths, it may or may not be
+# possible to lay them down so that they form a triangle when their ends are touching. For example, if all of the straws
+# have a length of 6 inches. then one can easily construct an equilateral triangle using them. However, if one straw is
+# 6 inches. long, while the other two are each only 2 inches. long, then a triangle cannot be formed. In general, if any
+# one length is greater than or equal to the sum of the other two then the lengths cannot be used to form a triangle.
+# Otherwise they can form a triangle. Write a function that determines whether or not three lengths can form a triangle.
+# The function will take 3 parameters and return a Boolean result. In addition, write a program that reads 3 lengths
+# from the user and demonstrates the behaviour of this function.
+
+def triangle_check(a, b, c):
+    check = True
+    if a >= b + c or b >= a + c or c >= a + b:
+        check = False
+    return check
+
+
+def main():
+    print('Is it a valid triangle?')
+    num1, num2, num3 = input('Please input 3 side-lengths to check if they can form a triangle: ').split()
+    print('Answer: ', triangle_check(int(num1), int(num2), int(num3)))
+
+
 # Exercise 65:  The Twelve Days of Christmas - The Twelve Days of Christmas is a repetitive song (lyrics available
 # online) that describes an increasingly long list of gifts sent to one’s true love on each of 12 days. A single gift is
 # sent on the first day. A new gift is added to the collection on each additional day, and then the complete collection
